3cd6ae66bd378ebf1a360d19f064e8c906ab6599
Kartik Kumar <83923172+kartik11721@users.noreply.github.com>
2021-06-15 16:00:59 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import re
+
+email = input('Enter A Email : ')
+pattern = re.compile(r'(^[a-zA-Z0-9_.!$&]+@[a-zA-Z]+\.[a-zA-Z]+$)')
+check = pattern.search(email)
+
+if check == None:
+    print('invalid email')
+else:
+    print('valid email')
